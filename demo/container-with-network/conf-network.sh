1fb31d62ce851a4d95ab0e8542be98adfea53116
Florian Arthofer <florian.arthofer@dynatrace.com>
2019-02-06 21:19:36 +0100

    added basic network config script

---
+++ demo/container-with-network/conf-network.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Run me with sudo, you fool!"
+    exit 1
+fi
+
+do_what="$1"
+
+# Show me what you do
+set -x
+
+function create {
+    # Create bridge
+    ip link add name runc0 type bridge
+    ip link set runc0 up
+    # Give bridge an IP address
+    ip addr add 192.168.10.1/24 dev runc0
+    # Create veth interface
+    ip link add name veth-host type veth peer name veth-guest
+    ip link set veth-host up
+    # Add veth interface to the bridge we created
+    ip link set veth-host master runc0
+    # Create network namespace
+    ip netns add runc
+    # Put veth interface for guest in created network namespace
+    ip link set veth-guest netns runc
+    # Configure veth interface
+    ip netns exec runc ip link set veth-guest name eth1
+    ip netns exec runc ip addr add 192.168.10.101/24 dev eth1
+    ip netns exec runc ip link set eth1 up
+    ip netns exec runc ip route add default via 192.168.10.1
+}
+
+function delete {
+    ip link delete veth-host
+    ip netns delete runc
+    ip link delete runc0
+}
+
+$do_what
